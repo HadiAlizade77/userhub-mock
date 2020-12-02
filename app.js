33b91ad56617c604907a7369aaa9cf57d2c63306
Hadi Alizadeh <hadi@atlann.co>
2020-12-02 12:12:21 +0330

    fixed bugs

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //CONFIG
 const dynamicApi = 'http://127.0.0.1:8000/api/planning_dynamic_endpoint'
 const toolURL = 'http://127.0.0.1:8000/'
-
 //
 const axios = require('axios')
 const express = require('express')
@@ -11,35 +10,37 @@ const fs = require('fs');
 let responses = JSON.parse(fs.readFileSync('responsesMock.json'));
 
 app.get('/', (req, res) => {
-
-  console.log('Hi :) I sent a response to your server')
-  let request_code = req.query.request_code ;
-  let payload_code = req.query.payload_code;
+  let request_code = req.query.request_code || null;
   let request = responses.auth[getRandomInt(3)]
   request.request_code = request_code
-  console.log(request)
-
-  axios.post(dynamicApi,request).then((response)=>{
-    res.redirect(toolURL)
-    console.log(response.data)
-  }).catch((err)=>{
-    res.send('sorry but youre not a valid user :D')
-    if(typeof err.response !== 'undefined'){
-      console.log('message:' + err.response.data.message)
-      console.log('file:' + err.response.data.file)
-      console.log('line:' + err.response.data.line)
-    }
-    else {
-      console.log('\x1b[31m', 'BUT your server didnt send any response')  //cyan
-    }
-  })
+  if (request_code !== null){
+    console.log('Hi :) I sent this response to your server')
+    console.log(request)
+    axios.post(dynamicApi,request).then((response)=>{
+      res.redirect(toolURL)
+      console.log(response.data)
+    }).catch((err)=>{
+      res.send('youre server didnt respond with 200')
+      console.log('youre server didnt respond with 200')
+      if(typeof err.response !== 'undefined'){
+        console.log('message:' + err.response.data.message)
+        console.log('file:' + err.response.data.file)
+        console.log('line:' + err.response.data.line)
+      }
+      else {
+        console.log('\x1b[31m', 'BUT your server didnt respond')  //cyan
+      }
+    })
+  }else{
+    res.send('check your console for info')
+    console.log('\x1b[31m', '')  //cyan
+  }
 })
-
-app.listen(port, () => {
-  console.log(`UserHub mock listening at http://localhost:${port}`)
-})
-
 ///helpers
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max)) | 0 ;
 }
+
+app.listen(port, () => {
+  console.log(`UserHub mock listening at http://localhost:${port}`)
+})
